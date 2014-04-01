5314337d127f021c9810e7213bb2e19e4137f8c6
stasdimos <stas.dimos@icloud.com>
2014-04-01 16:21:05 -0400

    all updates

--- crm.rb
+++ crm.rb
@@ -1,7 +1,26 @@
 require 'sinatra'
 
 get '/' do
+  @crm_app_name = "My CRM"
   erb :index
 end
 
+get "/contacts" do
+   @contacts_view = "Contacts"
+   erb :contacts
+end
 
+get "/contactsadd" do
+  @contacts_add = "Add a Contact!"
+  erb :contactsadd
+end
+
+get "/contactsid" do
+  @contacts_id = "View Contact ID!!"
+  erb :contactsid
+end
+
+get "/contactsedit" do
+  @contacts_edit = "Edit a Contact!!!"
+  erb :contactsedit
+end
